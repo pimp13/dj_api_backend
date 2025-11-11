1ee878412f1f668b5224dc6c5de3c8beee411bef
pimp13 <pimp.puma.13@gmail.com>
2025-11-11 14:36:16 +0330

    Refactor user registration view to use CreateAPIView for improved functionality and validation. Introduce custom JWT token serializer and view to include additional user information in the token response. Update permission settings to allow public access.

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,26 +1,47 @@
-from dataclasses import asdict
-from django.http.request import HttpRequest
 from rest_framework import status
 from rest_framework.exceptions import JsonResponse
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from auth_app.dto.register import RegisterDto
-from auth_app.serializers import RegisterSerializer, UserSerializer
+from auth_app.serializers import RegisterSerializer
 from auth_app.service import AuthService
+from utils.response import SuccessResponse
 
 
-class RegisterView(APIView):
+class RegisterView(CreateAPIView):
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
 
-    def post(self, request):
-        try:
-            bodyData = RegisterDto(
-                username=request.data["username"],
-                email=request.data["email"],
-                password=request.data["password"],
-            )
-            result = AuthService.register(bodyData)
-            return JsonResponse(result)
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
 
-        except ValueError as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        if serializer.is_valid():
+            serializer.save()
+            return SuccessResponse(
+                data=serializer.data, message="ثبت نام کاربر موفقیت آمیز بود"
+            ).to_response()
+        return ErrorResponse(
+            data=serializer.errors, message="خطا در ثبت نام"
+        ).to_response()
+
+
+# Custom JWT login response
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        # می‌توان اطلاعات اضافی اضافه کرد
+        token["username"] = user.username
+        return token
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
