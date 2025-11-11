d70808b3b44ed3231bf2b999bc3b8a21c32c03d9
pimp13 <pimp.puma.13@gmail.com>
2025-11-11 17:33:26 +0330

    Update middleware settings in settings.py to include a TODO comment for future implementation of JWT authentication middleware.

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "middlewares.jwt_auth.CookieJWTAuthenticationMiddleware",
+    "middlewares.jwt_auth.CookieJWTAuthenticationMiddleware",  # TODO: BUGE bayad skhte beshe
 ]
 
 ROOT_URLCONF = "config.urls"
